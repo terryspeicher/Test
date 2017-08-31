30cbba13d0f7ee94032a656eb8d5601599d73a5f
TBone <dnr@tspeicher.com>
2017-08-30 21:37:39 -0600

    First (test) commit

---
+++ Test/src/systems/outofcontrol/test/DemoTest.java
@@ -0,0 +1,17 @@
+package systems.outofcontrol.test;
+
+import java.applet.Applet;
+
+public class DemoTest extends Applet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
